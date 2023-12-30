02532ecc01d0cacf47a1ea601a000d446675a95b
Steffen Sanwald <security-online@steffen-sanwald.de>
2023-12-31 00:07:19 +0100

    simplified setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Note: To use the 'upload' functionality of this file, you must:
-#   $ pipenv install twine --dev
+from setuptools import setup, find_packages
 
-import io
-import os
-import sys
-from shutil import rmtree
-
-from setuptools import find_packages, setup, Command
-
-# Package meta-data.
-NAME = 'cython2skeleton'
-DESCRIPTION = 'Quickly gather highlevel insights into cython compiled executable/shared library.'
-URL = 'https://github.com/steffen-sanwald/cython2skeleton'
-AUTHOR = 'Steffen Sanwald'
-REQUIRES_PYTHON = '>=3.11.0'
-VERSION = '0.1.0'
-
-# What packages are required for this module to be executed?
-REQUIRED = [
-    'fire',
-    'binary2strings'
-]
-
-# What packages are optional?
-EXTRAS = {
-    # 'fancy feature': ['django'],
-}
+setup(
+    name='cython2skeleton',
+    version='0.1.0',
+    url='https://github.com/steffen-sanwald/cython2skeleton.git',
+    author='Steffen Sanwald',
+    author_email='author@gmail.com',
+    description='Quickly gather highlevel insights into cython compiled executable/shared library.',
+    packages=find_packages(),
+    install_requires=['fire', 'binary2strings'],
+)
